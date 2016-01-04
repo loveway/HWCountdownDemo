c7d15f4a130bb111e71d48b946b09abb4a6f45c0
chenhongwei <chenhongwei@igancao.com>
2016-01-04 21:33:14 +0800

    first commit

---
+++ HWCountdownDemo/UIButton+countDown.h
@@ -0,0 +1,24 @@
+//
+//  UIButton+countDown.h
+//  LiquoriceDoctorProject
+//
+//  Created by HenryCheng on 15/12/4.
+//  Copyright © 2015年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (countDown)
+
+/**
+ *  倒计时按钮
+ *
+ *  @param timeLine 倒计时总时间
+ *  @param title    还没倒计时的title
+ *  @param subTitle 倒计时中的子名字，如时、分
+ *  @param mColor   还没倒计时的颜色
+ *  @param color    倒计时中的颜色
+ */
+- (void)startWithTime:(NSInteger)timeLine title:(NSString *)title countDownTitle:(NSString *)subTitle mainColor:(UIColor *)mColor countColor:(UIColor *)color;
+
+@end
---
+++ HWCountdownDemo/UIButton+countDown.m
@@ -0,0 +1,45 @@
+//
+//  UIButton+countDown.m
+//  LiquoriceDoctorProject
+//
+//  Created by HenryCheng on 15/12/4.
+//  Copyright © 2015年 iMac. All rights reserved.
+//
+
+#import "UIButton+countDown.h"
+
+@implementation UIButton (countDown)
+
+- (void)startWithTime:(NSInteger)timeLine title:(NSString *)title countDownTitle:(NSString *)subTitle mainColor:(UIColor *)mColor countColor:(UIColor *)color {
+    
+    //倒计时时间
+    __block NSInteger timeOut = timeLine;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    //每秒执行一次
+    dispatch_source_set_timer(_timer, dispatch_walltime(NULL, 0), 1.0 * NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(_timer, ^{
+        
+        //倒计时结束，关闭
+        if (timeOut <= 0) {
+            dispatch_source_cancel(_timer);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.backgroundColor = mColor;
+                [self setTitle:title forState:UIControlStateNormal];
+                self.userInteractionEnabled = YES;
+            });
+        } else {
+            int seconds = timeOut % 60;
+            NSString *timeStr = [NSString stringWithFormat:@"%0.2d", seconds];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.backgroundColor = color;
+                [self setTitle:[NSString stringWithFormat:@"%@%@",timeStr,subTitle] forState:UIControlStateNormal];
+                self.userInteractionEnabled = NO;
+            });
+            timeOut--;
+        }
+    });
+    dispatch_resume(_timer);
+}
+
+@end
