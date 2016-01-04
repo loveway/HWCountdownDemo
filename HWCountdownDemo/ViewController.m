e2fbb408758c748e8df705587f46e47127c7d7e5
chenhongwei <chenhongwei@igancao.com>
2016-01-04 21:39:03 +0800

    1.4

--- HWCountdownDemo/ViewController.m
+++ HWCountdownDemo/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "UIButton+countDown.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *countdownBtn;
 
 @end
 
@@ -16,7 +18,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+
+}
+- (IBAction)countdownBtnClick:(UIButton *)sender {
+    [_countdownBtn startWithTime:5 title:@"获取验证码" countDownTitle:@"s" mainColor:[UIColor colorWithRed:84/255.0 green:180/255.0 blue:98/255.0 alpha:1.0f] countColor:[UIColor lightGrayColor]];
 }
 
 - (void)didReceiveMemoryWarning {
